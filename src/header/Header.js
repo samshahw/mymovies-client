a73c21c1f297dc34ed700920230b3240942ed239
Sam <46427402+samshahw@users.noreply.github.com>
2022-05-10 11:00:02 +0200

    Basic page structure

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,13 +1,32 @@
 import React from "react";
 import './Header.css';
+import {Link} from "react-router-dom";
 
 export default function Header() {
     return(
         <div id="header">
-            <h1 className="title">
-                MyMovies
-                <span className="title subtitle">- Internet movie database</span>
+            <h1 className="header-title">
+                <div>
+                    <span style={{color: 'yellow'}}>My</span>Movies
+                </div>
+                <div className="header-title header-subtitle">
+                    Internet movie database
+                </div>
             </h1>
+            <ul className="header-menus">
+                <li>
+                    <Link to="/movies">Movies</Link>
+                </li>
+                <li>
+                    <Link to="/directors">Directors</Link>
+                </li>
+                <li>
+                    <Link to="/admin">Admin</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </div>
     );
 }
